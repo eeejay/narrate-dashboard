3ac4c5c98147a3b90a7cd50d2342a82fd0233a0a
Eitan Isaacson <eitan@monotonous.org>
2017-02-09 16:38:55 -0800

    added executive summart and made it prettier

--- main.js
+++ main.js
@@ -12,7 +12,7 @@ function codeToName(code) {
 var bar;
 function populateTopLangs(rawStats) {
   stats = rawStats.slice();
-  stats.sort((a, b) => a.readerModeCount - b.readerModeCount);
+  stats.sort((a, b) => a.rmCount - b.rmCount);
   stats = stats.slice(stats.length - 16);
   let data = {
     labels: stats.map(s => `${s.label}`),
@@ -21,8 +21,8 @@ function populateTopLangs(rawStats) {
         name: "Narrate Supported",
         data: stats.map(s => {
           return {
-            value: s.narrateSuccess,
-            meta: (s.narrateSuccess * 100 / s.readerModeCount).toFixed(2) +
+            value: s.nSuccess,
+            meta: (s.nSuccess * 100 / s.rmCount).toFixed(2) +
               "% Narrate supported"
           };
         })
@@ -31,16 +31,16 @@ function populateTopLangs(rawStats) {
         name: "Narrate Unsupported",
         data: stats.map(s => {
           return {
-            value: s.narrateFail,
-            meta: (s.narrateFail * 100 / s.readerModeCount).toFixed(2) +
+            value: s.nFail,
+            meta: (s.nFail * 100 / s.rmCount).toFixed(2) +
               "% Narrate unsupported"
           };
         })
       }
     ]
   };
-  console.log("w", stats);
-  new Chartist.Bar("#topLangs .ct-chart", data, {
+
+  new Chartist.Bar("#topLangs", data, {
     horizontalBars: true,
     reverseData: false,
     stackBars: true,
@@ -66,22 +66,21 @@ function populateTopLangs(rawStats) {
 }
 
 function populateFailedLangs(stats, cutoff = 14) {
-  let pieData = stats.slice().sort((a, b) => b.narrateFail - a.narrateFail);
+  let pieData = stats.slice().sort((a, b) => b.nFail - a.nFail);
   let other = pieData.slice(cutoff).reduce((a, b) => {
     return {
       label: a.label,
-      narrateFail: a.narrateFail + b.narrateFail,
-      narrateFailShare: a.narrateFailShare + b.narrateFailShare
+      nFail: a.nFail + b.nFail,
+      nFailShare: a.nFailShare + b.nFailShare
     };
-  }, { label: "Other", narrateFail: 0, narrateFailShare: 0 });
+  }, { label: "Other", nFail: 0, nFailShare: 0 });
   pieData.splice(cutoff, pieData.length - cutoff, other);
-  console.log(pieData);
 
   let data = {
     labels: pieData.map(
-      l => `${l.label} (${(l.narrateFailShare * 100).toFixed(1)}%)`
+      l => `${l.label} (${(l.nFailShare * 100).toFixed(1)}%)`
     ),
-    series: pieData.map(l => l.narrateFail)
+    series: pieData.map(l => l.nFail)
   };
   var options = {
     plugins: [
@@ -90,14 +89,17 @@ function populateFailedLangs(stats, cutoff = 14) {
         seriesHeader: "",
         summary: ""
       })
-    ]
+    ],
+    width: "80%",
+    height: "80%"
   };
   var responsiveOptions = [
     [
       "screen and (min-width: 640px)",
       {
-        chartPadding: 30,
-        labelOffset: 100,
+        //chartPadding: 30,
+        //labelOffset: -10,
+        labelPosition: "outside",
         labelDirection: "explode",
         labelInterpolationFnc: function(value) {
           return value;
@@ -110,7 +112,7 @@ function populateFailedLangs(stats, cutoff = 14) {
     ]
   ];
 
-  new Chartist.Pie("#topFails .ct-chart", data, options, responsiveOptions);
+  new Chartist.Pie("#topFails", data, options, responsiveOptions);
 }
 
 function hd(v) {
@@ -125,16 +127,19 @@ function populateNarrateSpeakTime(stats) {
     labels,
     series: [
       series.map((v, i) => {
-        return { value: v / total * 100, meta: "Over " + hd(labels[i]) };
+        return { value: v / total * 100,
+          meta: i == 0 ? "Under 11 seconds" : "Over " + hd(labels[i]) };
       })
     ]
   };
 
-  new Chartist.Bar("#narrateSpeakTime .ct-chart", data, {
+  new Chartist.Bar("#narrateSpeakTime", data, {
     reverseData: false,
     axisX: {
       labelInterpolationFnc: function(v) {
-        console.log(v);
+        if (v == 300000) {
+          return "5:00+";
+        }
         let seconds = Math.round(v / 1000);
         if (seconds && seconds % 5 == 0) {
           return `${new Date(v).toLocaleFormat("%M:%S")}`;
@@ -166,6 +171,39 @@ function populateNarrateSpeakTime(stats) {
   });
 }
 
+function populateSummary(channel, version, rmStats, nStats) {
+  function setText(id, text) {
+    document.getElementById(id).textContent = text;
+  }
+
+  let channelName = document.getElementById("channel").selectedOptions[0].textContent;
+  setText("channelName", channelName);
+  setText("version", version);
+
+  let rmUsageTotal = rmStats.reduce((a, b) => a + b.rmCount, 0);
+  setText("rmUsageTotal", rmUsageTotal.toLocaleString());
+
+  let rmTopLang = rmStats.reduce(
+    (a, b) => a.rmShare > b.rmShare ? a : b, { rmShare: 0 });
+  setText("rmTopLang", rmTopLang.label);
+
+  let nSuccessRate = rmStats.reduce((a, b) => a + b.nSuccess, 0)/rmUsageTotal;
+  setText("nSuccessRate", (nSuccessRate*100).toFixed(1) + "%");
+
+  let nMostFailedLang = rmStats.reduce(
+    (a, b) => a.nFail > b.nFail ? a : b, { rmShare: 0 });
+  setText("nMostFailedLang", nMostFailedLang.label);
+
+  let nUsageTotal = nStats.values.reduce((a, b) => a + b);
+  setText("nUsageTotal", nStats.count.toLocaleString());
+
+  let nUnderSecondShare = (nStats.values[0] + nStats.values[1])/nUsageTotal;
+  setText("nUnderSecondShare", (nUnderSecondShare*100).toFixed(1) + "%");
+
+  let nOverMinuteShare = nStats.values.slice(6).reduce((a, b) => a + b)/nUsageTotal;
+  setText("nOverMinuteShare", (nOverMinuteShare*100).toFixed(1) + "%");
+}
+
 function currentVersions() {
   var nightly = Number(
     Telemetry
@@ -191,7 +229,7 @@ function evolutionToStats(evolution) {
     },
     0
   );
-  let totalNarrateFails = entries.reduce(
+  let totalnFails = entries.reduce(
     (a, b) => {
       let h = b[(1)].histogram();
       return a + (h.values[(0)] - h.values[(1)]);
@@ -201,26 +239,26 @@ function evolutionToStats(evolution) {
 
   return entries.map(([ lang, val ]) => {
     let values = val.histogram().values;
-    let readerModeCount = values[(0)];
-    let readerModeShare = values[(0)] / totalReaderUsage;
-    let narrateSuccess = values[(1)];
-    let narrateFail = values[(0)] - values[(1)];
-    let narrateFailShare = narrateFail / totalNarrateFails;
+    let rmCount = values[(0)];
+    let rmShare = values[(0)] / totalReaderUsage;
+    let nSuccess = values[(1)];
+    let nFail = values[(0)] - values[(1)];
+    let nFailShare = nFail / totalnFails;
     let label = codeToName(lang);
     return {
       lang,
       label,
-      readerModeCount,
-      readerModeShare,
-      narrateSuccess,
-      narrateFail,
-      narrateFailShare
+      rmCount,
+      rmShare,
+      nSuccess,
+      nFail,
+      nFailShare
     };
   });
 }
 
 function statsToCSV(stats) {
-  let keys = [ "lang", "readerModeCount", "narrateFail" ];
+  let keys = [ "lang", "rmCount", "nFail" ];
   return keys.map(k => JSON.stringify(k)).join(",") + "\n" + stats.map(stat => {
       return keys.map(k => JSON.stringify(stat[k])).join(",");
     }).join("\n");
@@ -230,8 +268,7 @@ let initPromise = new Promise(resolve => {
   Telemetry.init(resolve);
 });
 
-function getEvolution(channel, metric) {
-  var version = currentVersions()[channel];
+function getEvolution(channel, version, metric) {
   return new Promise(resolve => {
     Telemetry.getEvolution(channel, version, metric, {}, false, resolve);
   });
@@ -243,22 +280,23 @@ function main() {
     ? location.hash.substr(1)
     : document.getElementById("channel").value;
   initPromise.then(() => {
+    var version = currentVersions()[channel];
     Promise
       .all([
-        getEvolution(channel, "NARRATE_CONTENT_BY_LANGUAGE_2"),
-        getEvolution(channel, "NARRATE_CONTENT_SPEAKTIME_MS")
+        getEvolution(channel, version, "NARRATE_CONTENT_BY_LANGUAGE_2"),
+        getEvolution(channel, version, "NARRATE_CONTENT_SPEAKTIME_MS")
       ])
       .then(([ evoLang, evoSpeakTime ]) => {
         document.body.classList.remove("loading");
 
-        let stats = evolutionToStats(evoLang);
+        let rmStats = evolutionToStats(evoLang);
+        let nStats = evoSpeakTime[""].histogram();
         let csvlink = document.getElementById("csvlink");
-        csvlink.href = "data:text/csv," + encodeURIComponent(statsToCSV(stats));
-        csvlink.hidden = false;
-        populateTopLangs(stats);
-        populateFailedLangs(stats);
-
-        populateNarrateSpeakTime(evoSpeakTime[""].histogram());
+        csvlink.href = "data:text/csv," + encodeURIComponent(statsToCSV(rmStats));
+        populateTopLangs(rmStats);
+        populateFailedLangs(rmStats);
+        populateNarrateSpeakTime(nStats);
+        populateSummary(channel, version, rmStats, nStats);
       });
   });
 }
